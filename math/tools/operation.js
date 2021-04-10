ed6e8b49dfbfca5470e308bd32e5bb4f341c1839
PIG-cmd <71576224+PIG-cmd@users.noreply.github.com>
2021-04-10 11:53:43 +0800

    Add files via upload

---
+++ math/tools/operation.js
@@ -0,0 +1,20 @@
+//This JavaScript file must run on the browser which support ECMAScript6
+//Copyright (c) silly-20081126.github.io 2021
+//Content: fat-pig-2020@outlook.com
+//Server: Microsoft GitHub
+//Watch our project: https://github.com/silly-20081126/silly-20081126.github.io/
+//Version 1.0  Apr.4,2021
+//Author Yichen Xu , Hugang Li
+//JavaScript File Start
+
+
+//This file can only be referenced on one page
+//The url is " https://silly-20081126.github.io/math/tools/24drop.html"
+//Loading other JavaScript File
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET","https://silly-20081126.github.io/maths/js/24drop.js",false);
+xhr.send();
+eval(xhr.responseText);
+
+//JavaScript File END
